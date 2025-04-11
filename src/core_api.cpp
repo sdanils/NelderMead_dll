f2a8721c953a74ff06f012ec00c9b4abdfbd0ac6
Daniil Salomasov <danil.salomasov.np@gmail.com>
2025-04-11 16:42:28 +0300

    + fix: exponential calc, number variable + print_tree JSON + func for memory

--- src/core_api.cpp
+++ src/core_api.cpp
@@ -1,10 +1,16 @@
 #include "core_api.h"
 
-#include "classes.h"
+#include <nlohmann/json.hpp>
 
-/**
- * @details  В случае несоответствия числа переменных выбросит исключение.
- */
+#include "core_api.h"
+#include "expression_tree.h"
+
+using nlohmann::json;
+
+int get_number_variable(ExpressionTree* trее) {
+  return trее->get_number_variables();
+}
+
 double evaluate_tree(ExpressionTree* tree, const int number_variable,
                      const double* variables) {
   if (!tree->check_number_variables(number_variable)) {
@@ -13,3 +19,14 @@ double evaluate_tree(ExpressionTree* tree, const int number_variable,
 
   return tree->evaluate(number_variable, variables);
 };
+
+void delete_tree(ExpressionTree* tree) { delete tree; };
+
+char* print_tree(ExpressionTree* trее) {
+  string tree_str = trее->json_tree();
+  char* buffer = new char[tree_str.size() + 1];
+  strcpy_s(buffer, tree_str.size() + 1, tree_str.c_str());
+  return buffer;
+};
+
+void delete_string(char* tree_str) { delete[] tree_str; };
